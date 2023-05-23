71481c68cf6a5e6464c3b5a97402c14c6eb4241e
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-05-23 12:36:46 +0800

    added the line and text display.

--- testProgram/main.js
+++ testProgram/main.js
@@ -1,26 +1,51 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
 
-const sence = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGL1Renderer(); 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// setup renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-sence.add(cube);
+// setup camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set( 0, 0, 10 );
+camera.lookAt( 0, 0, 0 );
 
-camera.position.z = 5;
+// create the scene
+const scene = new THREE.Scene();
 
-function animate(){
-    requestAnimationFrame(animate);
-    renderer.render(sence, camera);
+// create a cube 
+const box_geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const boxMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( box_geometry, boxMaterial );
+scene.add( cube );
+
+// create the lines
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 }); 
+const points = [];
+points.push( new THREE.Vector3(-10, 0, 0));
+points.push( new THREE.Vector3(0, 10, 0));
+points.push( new THREE.Vector3(10, 0, 0));
+
+const line_geometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(line_geometry, lineMaterial)
+scene.add( line );
+
+//camera.position.z = 5;
+// https://discourse.threejs.org/t/html5-multiplayer-games-over-udp-client-server-using-geckos-io/15896
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.02;
+	cube.rotation.y += 0.02;
+
+	renderer.render( scene, camera );
 }
 
-animate();
+if ( WebGL.isWebGLAvailable() ) {
+    animate();
+} else{
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById('container').appendChild(warning);
+}
