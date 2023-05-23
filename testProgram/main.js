ea582a275f5ed49b8d8b8a1fe603a86d1b33e18b
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-05-23 10:16:12 +0800

    Added the animate function.

--- testProgram/main.js
+++ testProgram/main.js
@@ -1,1 +1,26 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
+const sence = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGL1Renderer(); 
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+sence.add(cube);
+
+camera.position.z = 5;
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(sence, camera);
+}
+
+animate();
